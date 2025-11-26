d041814c19f221c3d9f25f38b7418a9c53cee940
Manav Mishra Bytes <151657729+MANAV-MISHRA-BYTES@users.noreply.github.com>
2025-11-26 10:00:03 +0530

    2435. Paths In Matrix Whose Sum is Divisible By K
    Solution Added

---
+++ 2435.py
@@ -0,0 +1,75 @@
+'''
+2435. Paths In Matrix Whose Sum is Divisible By K
+
+You are given a 0-indexed m x n integer matrix grid and an integer k. You are currently at position (0, 0) and you want to reach position (m - 1, n - 1) moving only down or right.
+
+Return the number of paths where the sum of the elements on the path is divisible by k. Since the answer may be very large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[5,2,4],[3,0,5],[0,7,2]], k = 3
+Output: 2
+Explanation: There are two paths where the sum of the elements on the path is divisible by k.
+The first path highlighted in red has a sum of 5 + 2 + 4 + 5 + 2 = 18 which is divisible by 3.
+The second path highlighted in blue has a sum of 5 + 3 + 0 + 5 + 2 = 15 which is divisible by 3.
+Example 2:
+
+
+Input: grid = [[0,0]], k = 5
+Output: 1
+Explanation: The path highlighted in red has a sum of 0 + 0 = 0 which is divisible by 5.
+Example 3:
+
+
+Input: grid = [[7,3,4,9],[2,3,6,2],[2,3,7,0]], k = 1
+Output: 10
+Explanation: Every integer is divisible by 1 so the sum of the elements on every possible path is divisible by k.
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 5 * 104
+1 <= m * n <= 5 * 104
+0 <= grid[i][j] <= 100
+1 <= k <= 50
+
+'''
+
+
+#solution:
+
+class Solution:
+    def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
+        MOD = 10**9 + 7
+        m, n = len(grid), len(grid[0])
+
+        prev = [[0]*k for _ in range(n)]
+        curr = [[0]*k for _ in range(n)]
+
+        s = 0
+        for j in range(n):
+            s = (s + grid[0][j]) % k
+            prev[j][s] = 1
+
+        s = grid[0][0] % k
+
+        for i in range(1, m):
+            s = (s + grid[i][0]) % k
+            curr[0] = [0]*k
+            curr[0][s] = 1
+
+            for j in range(1, n):
+                curr[j] = [0]*k
+                val = grid[i][j]
+                for r in range(k):
+                    nr = (r + val) % k
+                    curr[j][nr] = (prev[j][r] + curr[j - 1][r]) % MOD
+
+            prev, curr = curr, prev
+
+        return prev[n - 1][0]
