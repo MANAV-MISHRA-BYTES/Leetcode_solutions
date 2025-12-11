d1a4675b1ea37a088bbe20dac00602e36e765ad8
Manav Mishra Bytes <151657729+MANAV-MISHRA-BYTES@users.noreply.github.com>
2025-12-11 13:49:49 +0530

    3531. Count Covered Buildings
    Solution updated

--- 3531.py
+++ 3531.py
@@ -65,3 +65,21 @@ All coordinates of buildings are unique.
 '''
 
 #solution:
+class Solution:
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
+        rmax = [0] * (n + 1)
+        rmin = [n + 1] * (n + 1)
+        cmax = [0] * (n + 1)
+        cmin = [n + 1] * (n + 1)
+        for x, y in buildings:
+            rmax[y] = max(rmax[y], x)
+            rmin[y] = min(rmin[y], x)
+            cmax[x] = max(cmax[x], y)
+            cmin[x] = min(cmin[x], y)
+
+        ans = 0
+        for x, y in buildings:
+            if rmin[y] < x < rmax[y] and cmin[x] < y < cmax[x]:
+                ans += 1
+
+        return ans
